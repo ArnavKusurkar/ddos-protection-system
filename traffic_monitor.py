4a306cb79bcd954c521144209cf71ddfb3dc0655
KnightRipper <arnavamol77@gmail.com>
2024-09-05 14:36:15 +0530

    Update traffic_monitor.py

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -1,1 +1,8 @@
+from scapy.all import sniff, IP
+from attack_detection import analyze_traffic
 
+def packet_callback(packet):
+    if IP in packet:
+        analyze_traffic(packet)
+
+sniff(filter="ip", prn=packet_callback, store=0)
